a39e1cd450c3283f18b23b77d55dc488579c70e4
Alfred Lau <alfred@sulphur.com.sg>
2022-03-28 14:54:28 +0800

    :rotating_light: tailwind config lint fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,32 +2,33 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   mode: 'jit',
   theme: {
     extend: {
       borderWidth: {
-        '1': '1px',
+        1: '1px',
       },
       colors: {
         blue: {
-          dark: "#252B5C",
-          DEFAULT: "#224DBFA",
+          dark: '#252B5C',
+          DEFAULT: '#224DBFA',
         },
-        white: "#FEFAFA",
-        teal: "#28C9AA",
+        white: '#FEFAFA',
+        orange: '#FF6153',
+        teal: '#28C9AA',
         grey: {
-          dark: "#707075",
-          light: "#EBe7e5",
-          DEFAULT: "#c8c8c8",
+          dark: '#707075',
+          light: '#EBe7e5',
+          DEFAULT: '#c8c8c8',
         },
       },
       fontFamily: {
-        'sans': ['ibm-plex-sans', ...defaultTheme.fontFamily.sans],
+        sans: ['ibm-plex-sans', ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [],
-}
+};
