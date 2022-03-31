b91a3939d50e0faa916b26c9eb1f9188d3758450
Alfred Lau <alfred@sulphur.com.sg>
2022-04-01 02:27:27 +0800

    :bug: actually responsive now

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,12 +1,6 @@
 import orderBy from 'lodash.orderby';
 
-export default function Image({
-  width,
-  height,
-  alternativeText,
-  url,
-  formats = [],
-}) {
+export default function Image({ width, height, alt, src, formats = [] }) {
   let srcSet = '';
   const orderedFormats = orderBy(formats, 'width');
 
@@ -14,15 +8,20 @@ export default function Image({
     srcSet += `${entry.url} ${entry.width}w, `;
   });
 
-  srcSet += url;
+  srcSet += src;
   return (
-    <span className="block w-[initial] h-[initial] absolute overflow-hidden inset-0">
+    <span
+      className="inline-flex justify-center items-center w-[initial] h-[initial] relative overflow-hidden inset-0"
+      style={{
+        width,
+        height,
+      }}>
       <img
         sizes="100vw"
         width={width}
         height={height}
-        src={url}
-        alt={alternativeText}
+        src={src}
+        alt={alt}
         srcSet={srcSet}
         className="object-cover object-center absolute w-0 h-0 min-w-full max-w-full min-h-full max-h-full"
       />
