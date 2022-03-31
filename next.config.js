3e41445a318ec63c276cb62f07b645e13a1cc35d
Alfred Lau <alfred@sulphur.com.sg>
2022-03-31 14:51:37 +0800

    :hammer: allow CDN remote images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['recyclopedia.ap-south-1.linodeobjects.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
