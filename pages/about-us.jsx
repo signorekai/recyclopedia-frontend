a79ea2a5e0b142bd26c957ea316683aec841aaca
Alfred Lau <alfred@sulphur.com.sg>
2022-04-08 18:29:48 +0800

    :lipstick: /about-us finished

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -1,9 +1,14 @@
 import Head from 'next/head';
 import qs from 'querystring';
 import Layout from '../components/Layout';
-import { staticFetcher } from '../lib/hooks';
+import Image from 'next/image';
+import { staticFetcher, useWindowDimensions } from '../lib/hooks';
+
+import AboutUsBg from '../assets/img/about-us.svg';
 
 export default function Page({ pageOptions }) {
+  const { width } = useWindowDimensions();
+
   console.log(pageOptions);
   return (
     <Layout showHeaderInitially={true} showHeaderOn="UP" hideHeaderOn="DOWN">
@@ -11,6 +16,43 @@ export default function Page({ pageOptions }) {
         <title>Recyclopedia - {pageOptions.title}</title>
         <meta name="description" content={pageOptions.description} />
       </Head>
+      <div className="bg-teal relative">
+        <div className="container">
+          {width < 1080 && (
+            <div className="-ml-8 md:mx-auto pt-4 pr-2">
+              <Image src={AboutUsBg} alt="" />
+            </div>
+          )}
+          <div className="lg:w-1/2 pb-7 pt-4 lg:py-20">
+            <h1 className="text-white">{pageOptions.introHeader}</h1>
+            <div
+              className="text-lg"
+              dangerouslySetInnerHTML={{
+                __html: pageOptions.description,
+              }}></div>
+          </div>
+        </div>
+        {width >= 1080 && (
+          <div className="absolute right-8 top-16">
+            <Image src={AboutUsBg} alt="" />
+          </div>
+        )}
+      </div>
+      <div className="container flex flex-col lg:flex-row mt-8 lg:mt-12 items-start">
+        <h2 className="text-black lg:w-1/4 lg:justify-start">Contact Us</h2>
+        <div className="w-full lg:w-1/3 lg:pr-8">
+          <p>
+            Spotted outdated information, or have suggestions for improvements?
+            Contact us via our feedback form.{' '}
+          </p>
+        </div>
+        <div className="w-full lg:w-1/4 mt-4 lg:mt-0">
+          <p>
+            For direct enquiries:{' '}
+            <a href="mailto:hello@recyclopedia.sg">hello@recyclopedia.sg</a>
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 }
