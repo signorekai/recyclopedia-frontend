b10f8311b57f6abd9a9cd21a8049aa9f01c4d87f
Alfred Lau <alfred@sulphur.com.sg>
2022-04-08 16:16:04 +0800

    :sparkles: init commit

---
+++ pages/about-us.jsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import qs from 'querystring';
+import Layout from '../components/Layout';
+import { ServerFetcher } from '../lib/hooks';
+
+export default function Page({ pageOptions }) {
+  console.log(pageOptions);
+  return (
+    <Layout showHeaderInitially={true} showHeaderOn="UP" hideHeaderOn="DOWN">
+      <Head>
+        <title>Recyclopedia - {pageOptions.title}</title>
+        <meta name="description" content={pageOptions.description} />
+      </Head>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const pageOptions = await ServerFetcher(
+    `${process.env.API_URL}/api/about-us-page?${qs.stringify({
+      populate: ['supporters'],
+    })}`,
+    process.env.API_KEY,
+  );
+
+  return { props: { pageOptions } };
+}
