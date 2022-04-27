f03372f802ebd2f4e60ec2756b486b7c5611551a
Alfred Lau <alfred@sulphur.com.sg>
2022-04-27 16:06:08 +0800

    :sparkles: added /api/bookmarks

---
+++ pages/api/bookmarks.js
@@ -0,0 +1,29 @@
+import { getToken } from 'next-auth/jwt';
+import qs from 'qs';
+
+export default async function handler(req, res) {
+  const token = await getToken({ req });
+  if (token) {
+    const bookmarks = await fetch(
+      `${process.env.API_URL}/api/bookmarks?${qs.stringify({
+        populate: '*',
+        filters: {
+          user: {
+            id: {
+              $eq: token.id,
+            },
+          },
+        },
+      })}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+        },
+      },
+    );
+    const result = await bookmarks.json();
+    res.status(200).json(result.data);
+  } else {
+    res.status(400);
+  }
+}
