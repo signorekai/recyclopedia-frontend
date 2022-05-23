49528f40c5088c821f9a5fa050d51bb0a92a0b6c
Alfred Lau <alfred@sulphur.com.sg>
2022-05-24 01:33:08 +0800

    :sparkles: add client side check for authentication

--- pages/account/index.jsx
+++ pages/account/index.jsx
@@ -1,12 +1,18 @@
-import { useSession, signOut } from 'next-auth/react';
+import { useSession, signOut, signIn } from 'next-auth/react';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 import Layout from '../../components/Layout';
 import { capitalise } from '../../lib/functions';
 
 export default function Page({ ...props }) {
   const { data: session, status: authStatus } = useSession();
-  console.log(88, session, authStatus);
+  useEffect(() => {
+    if (authStatus === 'unauthenticated') {
+      signIn();
+    }
+  }, []);
+
   return (
     <Layout>
       <section className="bg-teal">
