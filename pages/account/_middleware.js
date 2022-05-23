82e661051c54aaf042ba8e76bf50d99fd188c8cf
Alfred Lau <alfred@sulphur.com.sg>
2022-05-24 01:27:08 +0800

    :mute:

--- pages/account/_middleware.js
+++ pages/account/_middleware.js
@@ -3,7 +3,6 @@ import { withAuth } from 'next-auth/middleware';
 export default withAuth({
   callbacks: {
     authorized: ({ token, ...props }) => {
-      console.log(token, props);
       return token !== null;
     },
   },
