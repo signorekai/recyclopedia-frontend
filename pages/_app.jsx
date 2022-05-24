5267ca8730b527a30dee614f26c3a53c696c1a44
Alfred Lau <alfred@sulphur.com.sg>
2022-05-24 14:29:14 +0800

    :zap: added refetchInterval for session info

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,7 +5,7 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={session} refetchInterval={5 * 60}>
       <Component {...pageProps} />
     </SessionProvider>
   );
