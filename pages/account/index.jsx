db853406e0c30c027a6c35286704ac6caa66ac1e
Alfred Lau <alfred@sulphur.com.sg>
2022-05-26 00:57:46 +0800

    :bug: show loading when checking authentication

--- pages/account/index.jsx
+++ pages/account/index.jsx
@@ -14,22 +14,31 @@ export default function Page({ ...props }) {
   }, []);
 
   return (
-    <Layout>
+    <Layout
+      mainStyle={{ display: authStatus === 'authenticated' ? 'block' : 'flex' }}
+      footerStyle={{ marginTop: 0 }}>
       <Head>
         <title>Recyclopedia - Your account</title>
       </Head>
-      <section className="bg-teal">
-        <div className="container container--lg py-4 lg:py-12 flex flex-col md:flex-row justify-between">
-          <h3 className="text-white">
-            Hi {session && capitalise(session.user.name)}
-          </h3>
-          <div className="text-right text-white">
-            <button onClick={() => signOut({ callbackUrl: '/' })}>
-              <i className="far fa-sign-out" /> Logout
-            </button>
+      {authStatus === 'authenticated' && (
+        <section className="bg-teal">
+          <div className="container container--lg py-4 lg:py-12 flex flex-col md:flex-row justify-between">
+            <h3 className="text-white">
+              Hi {session && capitalise(session.user.name)}
+            </h3>
+            <div className="text-right text-white">
+              <button onClick={() => signOut({ callbackUrl: '/' })}>
+                <i className="far fa-sign-out" /> Logout
+              </button>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
+      {authStatus === 'loading' && (
+        <section className="w-full flex justify-center items-center">
+          <i className="fas fa-spinner text-5xl text-grey animate-spin"></i>
+        </section>
+      )}
     </Layout>
   );
 }
