35d4e40829951ce932ee124470c538d9781c3105
Alfred Lau <alfred@sulphur.com.sg>
2022-05-27 16:31:25 +0800

    :wrench: removed redirecting

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const nextConfig = {
   images: {
     domains: ['recyclopedia.ap-south-1.linodeobjects.com'],
   },
-  async redirects() {
-    return [
-      {
-        source: '/donate/:slug',
-        destination: '/resources/:slug',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
