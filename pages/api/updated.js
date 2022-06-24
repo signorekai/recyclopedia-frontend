f2ad835a33655abff0674ae9822fb116aa635f5f
Alfred Lau <alfred@sulphur.com.sg>
2022-06-24 18:15:22 +0800

    :sparkles: regen static page when content is updated!

---
+++ pages/api/updated.js
@@ -0,0 +1,86 @@
+export default async function handler(req, res) {
+  const { event, model, entry } = req.body;
+
+  console.log(req.headers.authorization, process.env.CONTENT_UPDATE_KEY);
+
+  if (
+    req.headers.authorization !== `Bearer ${process.env.CONTENT_UPDATE_KEY}`
+  ) {
+    return res.status(401).send('Error');
+  }
+
+  if (!event || !model || !entry) {
+    return res.status(401).send('Error');
+  }
+
+  let url = [];
+
+  switch (model) {
+    case 'article':
+    case 'item':
+    case 'resource':
+      url.push(`/${model}s/${entry.slug}`);
+      break;
+
+    case 'article-category':
+      url.push('/news-tips');
+      var items = entry.articles.map((article) => `/articles/${article.slug}`);
+      url = url.concat(items);
+      break;
+
+    case 'item-category':
+      url.push('/items');
+      var items = entry.items.map((item) => `/items/${item.slug}`);
+      url = url.concat(items);
+      break;
+
+    case 'resource-tag':
+      url.push('/donate', '/resources', '/shops');
+
+      var items = entry.resources.map(
+        (resource) => `/resources/${resource.slug}`,
+      );
+      url = url.concat(items);
+      break;
+
+    case 'about-us-page':
+      url.push('/about-us');
+      break;
+
+    case 'donate-page':
+      url.push('/donate');
+      break;
+
+    case 'faq':
+      url.push('/faq');
+      break;
+
+    case 'items-page':
+      url.push('/items');
+      break;
+
+    case 'news-and-tips-page':
+      url.push('/news-tips');
+      break;
+
+    case 'resource-page':
+      url.push('/resources');
+      break;
+
+    case 'shops-page':
+      url.push('/shops');
+      break;
+  }
+
+  try {
+    for (let index = 0; index < url.length; index++) {
+      await res.unstable_revalidate(url[index]);
+      console.log('success', url[index]);
+    }
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating');
+  }
+}
