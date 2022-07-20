27632ed5adba07f63994962a2338a3c0efc1b1ff
Alfred Lau <alfred@sulphur.com.sg>
2022-07-20 16:02:06 +0800

    :bug: HTTPS cookies apparently have a different name from HTTP cookies

--- middleware.js
+++ middleware.js
@@ -1,20 +1,18 @@
-import { NextResponse, userAgent, NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 import { decode } from 'next-auth/jwt';
 
 export async function middleware(req) {
   if (req.nextUrl.pathname.startsWith('/account')) {
     const sessionToken =
       req.headers.get('authorization') ||
-      req.cookies.get('next-auth.session-token');
-
+      req.cookies.get('next-auth.session-token') ||
+      req.cookies.get('__Secure-next-auth.session-token');
     console.log('cookies', req.cookies.entries());
 
     const token = await decode({
       token: sessionToken,
       secret: process.env.NEXTAUTH_SECRET,
     });
-
-    console.log('middleware', sessionToken, token);
 
     if (token === null) {
       const loginUrl = new URL('/login', req.url);
