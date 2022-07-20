6f3455c4f21dc35c4376366047758fdf5ccc977c
Alfred Lau <alfred@sulphur.com.sg>
2022-07-20 16:55:03 +0800

    :art: move to separate function export

--- pages/api/log.js
+++ pages/api/log.js
@@ -1,29 +1,16 @@
 import { checkHTTPMethod } from '../../lib/functions';
 import { userAgentFromString } from 'next/server';
-import Cookies from 'cookies';
+import { getOrSetVisitorToken } from '../../lib/analytics';
 
 export default async function handler(req, res) {
   checkHTTPMethod(res, req.method, ['POST']);
-  const cookies = new Cookies(req, res);
   const { device, browser, os, isBot } = userAgentFromString(
     req.headers['user-agent'],
   );
   const location = new URL(req.headers['referer']);
   const path = location.pathname;
 
-  let visitorId = cookies.get('v_id');
-  let date = new Date();
-  if (visitorId === undefined) {
-    visitorId =
-      date.getTime().toString(16) +
-      Math.floor(Math.random() * (999999 - 100000) + 100000).toString(16);
-  }
-
-  date.setTime(date.getTime() + 2 * 365 * 24 * 60 * 60 * 1000);
-  cookies.set('v_id', visitorId, {
-    expires: date,
-    sameSite: 'strict',
-  });
+  const visitorId = getOrSetVisitorToken(req, res);
 
   if (!isBot) {
     const response = await fetch(`${process.env.API_URL}/logs`, {
