b3812a4b4931f25e0a01ebfd46e4bf845d6903cd
Alfred Lau <alfred@sulphur.com.sg>
2022-11-21 17:28:06 +0800

    :lipstick: updated about-us page

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -17,26 +17,19 @@ export default function Page({ pageOptions }) {
         <meta name="description" content={description} />
       </Head>
       <div className="bg-teal relative">
-        <div className="container">
-          {width < 1080 && (
-            <div className="-ml-8 md:mx-auto pt-4 pr-2">
-              <Image src={AboutUsBg} alt="" />
-            </div>
-          )}
-          <div className="lg:w-1/2 pb-7 pt-4 lg:py-10">
-            <h1 className="text-white">{introHeader}</h1>
-            <div
-              className="text-lg"
-              dangerouslySetInnerHTML={{
-                __html: description,
-              }}></div>
-          </div>
-        </div>
-        {width >= 1080 && (
-          <div className="absolute right-8 top-16">
+        <div className="container lg:flex lg:py-12">
+          <div className="pt-4 pr-2 lg:pl-4 lg:pr-0 lg:pt-4 lg:order-2 lg:-mr-24">
             <Image src={AboutUsBg} alt="" />
           </div>
-        )}
+          <div className="lg:w-3/5 pb-7 pt-4 lg:py-10 lg:order-1">
+            <h2 className="text-black justify-start">{introHeader}</h2>
+            <h1
+              className="text-black"
+              dangerouslySetInnerHTML={{
+                __html: description,
+              }}></h1>
+          </div>
+        </div>
       </div>
       <div className="container">
         <div
@@ -46,13 +39,28 @@ export default function Page({ pageOptions }) {
       </div>
       <div className="container flex flex-col lg:flex-row mt-8 lg:mt-14 lg:mb-6 items-start">
         <h2 className="text-black lg:w-1/4 lg:justify-start">Contact Us</h2>
-        <div className="w-full lg:w-1/3 lg:pr-8">
+        <div className="w-full">
           <p>
             Spotted outdated information, or have suggestions for improvements?
             Contact us via our feedback form.{' '}
           </p>
+          <p>
+            For direct enquiries:{' '}
+            <a href="mailto:hello@recyclopedia.sg">hello@recyclopedia.sg</a>
+          </p>
         </div>
-        <div className="w-full lg:w-1/4 mt-4 lg:mt-0">
+      </div>
+      <div className="container divider-b mt-16"></div>
+      <div className="container flex flex-col lg:flex-row mt-8 lg:mt-14 lg:mb-6 items-start">
+        <h2 className="text-black lg:w-1/4 lg:justify-start">
+          Be part of the project
+        </h2>
+        <div className="w-full">
+          <p>
+            If you are passionate about zero waste and would like to contribute,
+            drop us a line at{' '}
+            <a href="mailto:hello@recyclopedia.sg">hello@recyclopedia.sg</a>
+          </p>
           <p>
             For direct enquiries:{' '}
             <a href="mailto:hello@recyclopedia.sg">hello@recyclopedia.sg</a>
@@ -60,29 +68,32 @@ export default function Page({ pageOptions }) {
         </div>
       </div>
       {supporters && supporters.length > 1 && (
-        <div className="container flex flex-col lg:flex-row mt-8 lg:mt-12 items-start">
-          <h3 className="text-black lg:w-1/4 lg:justify-start">
-            Proudly Supported by
-          </h3>
-          <div className="flex-1 mt-2 lg:mt-0 flex lg:grid items-center grid-cols-3 gap-x-4 gap-y-2">
-            {supporters.map((supporter) => (
-              <a
-                href={supporter.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                key={supporter.name}>
-                <NewImage
-                  className="max-h-[55px]"
-                  width={supporter.image.width}
-                  height={supporter.image.height}
-                  layout="fixed"
-                  placeholder={supporter.image.placeholder}
-                  source={supporter.image}
-                />
-              </a>
-            ))}
+        <>
+          <div className="container divider-b mt-16"></div>
+          <div className="container flex flex-col lg:flex-row mt-8 lg:mt-12 items-start">
+            <h2 className="text-black lg:w-1/4 lg:justify-start">
+              Proudly Supported by
+            </h2>
+            <div className="flex-1 mt-2 lg:mt-0 flex lg:grid items-center grid-cols-3 gap-x-4 gap-y-2">
+              {supporters.map((supporter) => (
+                <a
+                  href={supporter.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  key={supporter.name}>
+                  <NewImage
+                    className="max-h-[55px]"
+                    width={supporter.image.width}
+                    height={supporter.image.height}
+                    layout="fixed"
+                    placeholder={supporter.image.placeholder}
+                    source={supporter.image}
+                  />
+                </a>
+              ))}
+            </div>
           </div>
-        </div>
+        </>
       )}
     </Layout>
   );
