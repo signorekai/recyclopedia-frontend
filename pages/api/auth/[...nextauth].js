38fad4a5c8e24c065f09188d8cf37507182cad22
Alfred Lau <alfred@sulphur.com.sg>
2022-11-23 11:25:05 +0800

    :alembic: testing email linking

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -42,10 +42,12 @@ const options = {
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      allowDangerousEmailAccountLinking: true,
       authorization: {
         params: {
           prompt: 'consent',
