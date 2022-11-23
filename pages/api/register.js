ad9ba938365e563fefd747812fb331d10e57bed4
Alfred Lau <alfred@sulphur.com.sg>
2022-11-23 21:55:08 +0800

    :bug: check for email before registering

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,4 +1,5 @@
 import { object, string } from 'yup';
+import { staticFetcher } from '../../lib/hooks';
 
 export const RegisterSchema = object({
   email: string().email('Email is invalid').required('Email required'),
@@ -15,30 +16,54 @@ export default async function handler(req, res) {
   const validation = await RegisterSchema.isValid(body);
   if (validation) {
     // @todo register user!
-    const response = await fetch(`${process.env.API_URL}/auth/local/register`, {
-      method: 'POST',
-      body: JSON.stringify({
-        username: body.email,
-        email: body.email,
-        password: body.password,
-        name: body.name,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
+    const data = await staticFetcher(
+      `${process.env.API_URL}/users`,
+      process.env.API_KEY,
+      {
+        filters: {
+          email: {
+            $eq: body.email,
+          },
+        },
       },
-    });
+    );
 
-    const results = await response.json();
-
-    if (results.hasOwnProperty('error') === false) {
-      res.status(200).json({ success: true, data: results.data });
-    } else {
+    if (data.length > 0) {
       res.status(400).json({
         success: false,
         data: {
-          error: results.error.message,
+          error: 'Email is already used.',
         },
       });
+    } else {
+      const response = await fetch(
+        `${process.env.API_URL}/auth/local/register`,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            username: body.email,
+            email: body.email,
+            password: body.password,
+            name: body.name,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      const results = await response.json();
+
+      if (results.hasOwnProperty('error') === false) {
+        res.status(200).json({ success: true, data: results.data });
+      } else {
+        res.status(400).json({
+          success: false,
+          data: {
+            error: results.error.message,
+          },
+        });
+      }
     }
   } else {
     // @todo test invalid
