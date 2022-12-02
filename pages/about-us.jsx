5e962a75af422416de6976ad827da5c8b31e563c
Alfred Lau <alfred@sulphur.com.sg>
2022-12-03 03:25:08 +0800

    :lipstick:

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -38,63 +38,66 @@ export default function Page({ pageOptions }) {
         <div
           className="pb-7 pt-4 lg:py-14 article-body"
           dangerouslySetInnerHTML={{ __html: bodyText }}></div>
-        <div className="divider-b"></div>
       </div>
-      <div className="container flex flex-col lg:flex-row mt-8 lg:mt-14 lg:mb-6 items-start">
-        <h2 className="text-black lg:w-1/4 lg:justify-start">Contact Us</h2>
-        <div className="w-full md:w-3/4 pl-4">
-          <p>
-            Spotted outdated information, or have suggestions for improvements?
-            <br />
-            Contact us via our <Link href="/feedback">feedback form</Link>.
-          </p>
-          <p>
-            For direct enquiries:{' '}
-            <a href="mailto:hello@recyclopedia.sg">hello@recyclopedia.sg</a>
-          </p>
-        </div>
-      </div>
-      <div className="container divider-b mt-12 lg:mt-16"></div>
-      <div className="container flex flex-col lg:flex-row mt-8 lg:mt-14 lg:mb-6 items-start">
-        <h2 className="text-black lg:w-1/4 lg:justify-start">
-          Be part of the project
-        </h2>
-        <div className="w-full md:w-3/4 pl-4">
-          <p>
-            If you are passionate about zero waste and would like to contribute,
-            drop us a line at{' '}
-            <a href="mailto:hello@recyclopedia.sg">hello@recyclopedia.sg</a>
-          </p>
-        </div>
-      </div>
-      {supporters && supporters.length > 1 && (
-        <>
-          <div className="container divider-b mt-12 lg:mt-16"></div>
-          <div className="container flex flex-col lg:flex-row mt-8 lg:mt-12 items-start">
-            <h2 className="text-black lg:w-1/4 lg:justify-start">
-              Proudly Supported by
-            </h2>
-            <div className="flex-1 mt-2 lg:mt-0 flex lg:grid items-center grid-cols-3 gap-x-4 gap-y-2">
-              {supporters.map((supporter) => (
-                <a
-                  href={supporter.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  key={supporter.name}>
-                  <NewImage
-                    className="max-h-[55px]"
-                    width={supporter.image.width}
-                    height={supporter.image.height}
-                    layout="fixed"
-                    placeholder={supporter.image.placeholder}
-                    source={supporter.image}
-                  />
-                </a>
-              ))}
-            </div>
+      <div className="my-8 md:my-20">
+        <div className="container max-w-[72rem] divider-b "></div>
+        <div className="container flex flex-col lg:flex-row mt-8 lg:my-12 items-start">
+          <h2 className="text-black lg:w-1/4 lg:justify-start">Contact Us</h2>
+          <div className="w-full md:w-3/4 md:pl-4">
+            <p>
+              Spotted outdated information, or have suggestions for
+              improvements?
+              <br />
+              Contact us via our <Link href="/feedback">feedback form</Link>.
+            </p>
+            <p>
+              For direct enquiries:{' '}
+              <a href="mailto:hello@recyclopedia.sg">hello@recyclopedia.sg</a>
+            </p>
           </div>
-        </>
-      )}
+        </div>
+        <div className="container max-w-[72rem] divider-b mt-6"></div>
+        <div className="container flex flex-col lg:flex-row mt-8 lg:my-12 items-start">
+          <h2 className="text-black lg:w-1/4 lg:justify-start">
+            Be part of the project
+          </h2>
+          <div className="w-full md:w-3/4 md:pl-4">
+            <p>
+              If you are passionate about zero waste and would like to
+              contribute, drop us a line at{' '}
+              <a href="mailto:hello@recyclopedia.sg">hello@recyclopedia.sg</a>
+            </p>
+          </div>
+        </div>
+        {supporters && supporters.length > 1 && (
+          <>
+            <div className="container divider-b mt-12 lg:mt-16"></div>
+            <div className="container flex flex-col lg:flex-row mt-8 lg:mt-12 items-start">
+              <h2 className="text-black lg:w-1/4 lg:justify-start">
+                Proudly Supported by
+              </h2>
+              <div className="flex-1 mt-2 lg:mt-0 flex lg:grid items-center grid-cols-3 gap-x-4 gap-y-2">
+                {supporters.map((supporter) => (
+                  <a
+                    href={supporter.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    key={supporter.name}>
+                    <NewImage
+                      className="max-h-[55px]"
+                      width={supporter.image.width}
+                      height={supporter.image.height}
+                      layout="fixed"
+                      placeholder={supporter.image.placeholder}
+                      source={supporter.image}
+                    />
+                  </a>
+                ))}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </Layout>
   );
 }
