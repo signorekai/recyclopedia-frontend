839695c5a43a69d81454eb4bdf07bedcbf2ee9b9
Alfred Lau <alfred@sulphur.com.sg>
2023-01-26 01:23:29 +0800

    :lipstick: adjusted title

--- pages/search.jsx
+++ pages/search.jsx
@@ -380,7 +380,7 @@ const MultiSearchType = ({ type, query, data, pageOptions }) => {
 
 export default function Page(props) {
   return (
-    <Layout title={`Recyclopedia - Search Results for "${props.query}"`}>
+    <Layout title={`Search Results for "${props.query}"`}>
       {props.success && props.type.length === 1 && (
         <SingleSearchType
           type={props.type[0]}
