579642acaf7b5fee364e6ad7f8f8b150aeb2aa74
Alfred Lau <alfred@sulphur.com.sg>
2023-03-21 18:25:39 +0800

    :sparkles: only show on scroll

--- components/StickyNewsletterButton.jsx
+++ components/StickyNewsletterButton.jsx
@@ -20,26 +20,31 @@ export default function StickyNewsletterButton() {
 
   useEffect(() => {
     // https://www.w3schools.com/js/js_cookies.asp
-    const hasCookie = (cname) => {
-      let name = cname + '=';
-      let decodedCookie = decodeURIComponent(document.cookie);
-      let ca = decodedCookie.split(';');
-      for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-          c = c.substring(1);
+    const _handleScroll = () => {
+      const hasCookie = (cname) => {
+        let name = cname + '=';
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return true;
+            // return c.substring(name.length, c.length);
+          }
         }
-        if (c.indexOf(name) == 0) {
-          return true;
-          // return c.substring(name.length, c.length);
-        }
-      }
-      return false;
-    };
+        return false;
+      };
 
-    if (hasCookie('hideStickyBtn') === false) {
-      setShowBtn(true);
-    }
+      if (hasCookie('hideStickyBtn') === false) {
+        setShowBtn(true);
+      }
+    };
+    window.addEventListener('scroll', _handleScroll);
+
+    return () => window.removeEventListener('resize', _handleScroll);
   }, []);
 
   return (
